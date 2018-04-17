cc8752bf9cbfe6d59df896b5768a50e1c57b2452
xiaobo <582999769@qq.com>
2018-04-17 11:22:13 +0800

    增加了登录

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,8 @@ const User = require('../models/User');
 // 向外暴露这个文件
 module.exports = router;
 
-/* 注册 */
+/* 注册--------------------------------------- */
 router.post('/', function (req, res, next) {
-  console.log("开始注册。。。");
   //得到用户填写的东西
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
@@ -41,7 +40,6 @@ router.post('/', function (req, res, next) {
 
         // 如果该用户已经存在，做以下处理
         if (user != "") {        
-          console.log("搜索结果："+user);
           res.json(obj[1]);
         } else {
 
@@ -53,7 +51,7 @@ router.post('/', function (req, res, next) {
             if (err) {
               next();
             } else {
-              console.log(result);
+              console.log("注册成功：" + result.user_name);
               res.json(obj[0]);
             }
             
@@ -65,3 +63,52 @@ router.post('/', function (req, res, next) {
   });
 });
 
+/* 登录--------------------------------------- */
+router.post('/login', function (req, res, next) {
+  //得到用户填写的东西
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+  
+  let obj = [{
+                "result": "1",
+                "message": "登录成功"
+              },
+              {
+                "result": "-1",
+                "message": "用户名或密码错误，请重试"
+              },
+              {
+                "result": "-2",
+                "message": "用户名或密码为空，请重试"
+              }];
+
+    //得到表单之后做的事情
+
+    // 排除空表单
+    if (fields.user_name =="" || fields.password == "") {
+      res.json(obj[2]);
+    } else {
+      let user_name = fields.user_name;
+      let password = fields.password;
+
+
+      // 查询一下，是否有这个用户
+      User.getUserByUserName(user_name, function (err, user) {
+        if (user == {}) {
+           res.json(obj[1]);
+        } else {
+          // 如果该用户已经存在，做以下处理
+          if (password == user[0].password) {
+            console.log("登录成功：" + user_name);
+            res.json(obj[0]);
+          } else {
+            res.json(obj[1]);
+          }
+        }
+        
+        
+      });
+    }
+    
+  });
+});
